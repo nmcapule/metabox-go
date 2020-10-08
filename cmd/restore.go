e5331574852da1f2e321bd2159d7e1a3ded0f877
Nathaniel Capule <nathaniel.capule@celconsulting.com>
2020-10-08 13:39:39 +0800

    Fix restore command to properly use tags flag.

--- cmd/restore.go
+++ cmd/restore.go
@@ -11,7 +11,7 @@ import (
 
 type Restore struct {
 	configPath string
-	flagTags   *[]string
+	flagTags   []string
 }
 
 func (cmd *Restore) Execute() error {
@@ -21,33 +21,39 @@ func (cmd *Restore) Execute() error {
 	}
 
 	var matchers []tracker.Predicate
-	for _, tag := range *cmd.flagTags {
+	for _, tag := range cmd.flagTags {
 		matchers = append(matchers, tracker.PredicateTag(tag))
 	}
 
 	item, err := box.DB.QueryLatest(matchers...)
 	if err != nil {
-		return fmt.Errorf("retrieving item tagged %+v: %v", *cmd.flagTags, err)
+		return fmt.Errorf("retrieving item tagged %+v: %v", cmd.flagTags, err)
 	}
 
 	return box.StartRestore(item)
 }
 
-var cmdRestore = &cobra.Command{
-	Use:   "restore",
-	Short: "Restore file from backup",
-	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		r := Restore{
-			configPath: args[0],
-			flagTags:   cmd.Flags().StringArrayP("tags", "t", nil, "Tag matchers"),
-		}
-		if err := r.Execute(); err != nil {
-			log.Fatalln(err)
-		}
-	},
-}
-
 func init() {
+	cmdRestore := &cobra.Command{
+		Use:   "restore",
+		Short: "Restore file from backup",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			tags, err := cmd.Flags().GetStringArray("tags")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			r := Restore{
+				configPath: args[0],
+				flagTags:   tags,
+			}
+			if err := r.Execute(); err != nil {
+				log.Fatalln(err)
+			}
+		},
+	}
+	cmdRestore.Flags().StringArrayP("tags", "t", nil, "Tag matchers")
+
 	root.AddCommand(cmdRestore)
 }
